425ef173d7708601539f993bbe96cee62db5ef38
Oom Lout <aaron@oomlout.com>
2024-11-11 11:50:44 +0000

    initial

--- working.py
+++ working.py
@@ -1,120 +1,150 @@
-import oom_kicad
-import oom_markdown
 import os
+import yaml
+import glob
 import copy
-import scad
+import jinja2    
+import time
+import qrcode
 
-#process
-#  locations set in working_parts.ods 
-#  export to working_parts.csv
-#  put components on the right side of the board
-#  run this script
+
+# local oomp file
+utility_name = os.path.dirname(__file__)
+#grab last directry before filename in utility_name
+utility_name = utility_name.split("\\")[-1]
+test_filename = f"configuration\\{utility_name}_configuration.yaml"
+if os.path.exists(test_filename):
+    file_configuration = test_filename
+else:
+    #default config file
+    folder_configuration = "configuration"
+    #add this files current loaction to the folder
+    folder_configuration = os.path.join(os.path.dirname(__file__), folder_configuration)
+    file_configuration = os.path.join(folder_configuration, "configuration.yaml")
+    #import templates
+with open(file_configuration, 'r') as stream:
+    try:
+        configuration = yaml.load(stream, Loader=yaml.FullLoader)
+    except yaml.YAMLError as exc:   
+        print(exc)
+
+pass   
+
+ 
+    
+
+
+
 
 def main(**kwargs):
-    #place_parts(**kwargs)
-    #make_readme(**kwargs)
-    scad.make_scad(**kwargs)
     
+    
+    folder = kwargs.get("folder", f"os.path.dirname(__file__)/parts")
+    folder = folder.replace("\\","/")
+    
+    kwargs["file_template_list"] = configuration
+    print(f"oomlout_oomp_utility_label_generation for folder: {folder}")
+    create_recursive(**kwargs)
+    
+def create_recursive(**kwargs):
+    folder = kwargs.get("folder", os.path.dirname(__file__))
+    kwargs["folder"] = folder
+    folder_template_absolute = kwargs.get("folder_template_absolute", "")
+    kwargs["folder_template_absolute"] = folder_template_absolute
+    filter = kwargs.get("filter", "")
+    count = 0
+    for item in os.listdir(folder):
+        if filter in item:
+            item_absolute = os.path.join(folder, item)
+            if os.path.isdir(item_absolute):
+                #if working.yaml exists in the folder
+                if os.path.exists(os.path.join(item_absolute, "working.yaml")):
+                    kwargs["directory"] = item_absolute
+                    create(**kwargs)
+                    count += 1
+                    if count % 100 == 0:
+                        print(f"count: {count}")
+
+def create(**kwargs):
+    directory = kwargs.get("directory", os.getcwd())    
+    kwargs["directory"] = directory
+    file_template_list = kwargs.get("configuration", configuration)
+    kwargs["configuration"] = file_template_list
+    generate_qr_code_generic(**kwargs)
     
 
-def make_readme(**kwargs):
-    os.system("generate_resolution.bat")
-    oom_markdown.generate_readme_project(**kwargs)
-    #oom_markdown.generate_readme_teardown(**kwargs)
+def generate_qr_code_generic(**kwargs):
+    import os
+    directory = kwargs.get("directory",os.getcwd())    
+    configuration = kwargs.get("configuration", "")
     
-def make_scad(**kwargs):
-    import opsc
-    import oobb 
-    import oobb_base
+    pass 
+    #load working.yaml from directory
+    with open(os.path.join(directory, "working.yaml"), 'r') as stream:
+        try:
+            working = yaml.load(stream, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:   
+            print(exc)
+    
+    # qr code for each entry in configuration
+    for item in configuration:
+        tag_id = item["id"]
+        tag_data = working.get(tag_id, "")
+        web_link = item.get("web_link", "http://oom.lt")
+        web_make = item.get("web", False)
+        file_name_base = item.get("file_name", f"qr_code\\{tag_id}")
+        if tag_data != "":
+            file_name = f"{file_name_base}.png"
+            file_name_web = f"{file_name_base}_web.png"
+            #add directory to file_name
+            file_name = os.path.join(directory, file_name)
+            file_name_web = os.path.join(directory, file_name_web)
 
-    kwargs["save_type"] = "none"
-    #kwargs["save_type"] = "all"
-    kwargs["size"] = "oobb"
-    kwargs["type"] = "oomlout_bolt_tool_funnel"
-    kwargs["width"] = 3
-    kwargs["height"] = 5
-    kwargs["thickness"] = 6
-     # default sets
-    width = kwargs.get("width", 3)
-    height = kwargs.get("height", 5)
-    thickness = kwargs.get("thickness", 3)
-    size = kwargs.get("size", "oobb")
-    pos = kwargs.get("pos", [0, 0, 0])
-    # extra sets
-    holes = kwargs.get("holes", True)
-    both_holes = kwargs.get("both_holes", True)    
-    kwargs["pos"] = pos
-    
-        # get the default thing
-    thing = oobb_base.get_default_thing(**kwargs)
-    th = thing["components"]
-    kwargs.pop("size","")
+            codes = []
+            codes.append([tag_data, file_name])
+            if web_make:
+                codes.append([f"{web_link}/{tag_data}", file_name_web])
 
-    th.append(oobb_base.get_comment("plate main","p"))
-    # add plate
-    p3 = copy.deepcopy(kwargs)
-    p3["type"] = "p"   
-    p3["shape"] = f"{size}_plate"
-    p3["width"] = width
-    p3["height"] = height  
-    p3["depth"] = thickness
-    p3["pos"] = pos
-    #p3["m"] = ""  
-    oobb_base.append_full(thing,**p3)      
-    #th.append(oobb_base.oobb_easy(**p3))
-    
-    # add holes
-    if holes:
-        th.append(oobb_base.get_comment("holes main","n"))
-        p3 = copy.deepcopy(kwargs)
-        p3["type"] = "n"
-        p3["shape"] = f"{size}_holes"
-        p3["width"] = width
-        p3["height"] = height
-        p3["pos"] = pos
-        p3["both_holes"] = both_holes
-        #p3["m"] = ""
-        oobb_base.append_full(thing,**p3)      
-        #th.extend(oobb_base.oobb_easy(**p3))   
-        
-        
-        save_type = kwargs.get("save_type", "all")
-        overwrite = True
-        modes = ["3dpr", "laser", "true"]
-        for mode in modes:
-            depth = thing.get(
-                "depth_mm", thing.get("thickness_mm", 3))
-            height = thing.get("height_mm", 100)
-            layers = depth / 3
-            tilediff = height + 10
-            start = 1.5
-            if layers != 1:
-                start = 1.5 - (layers / 2)*3
-            if "bunting" in thing:
-                start = 0.5
-            opsc.opsc_make_object(f'scad_output/{thing["id"]}/{mode}.scad', thing["components"], mode=mode, save_type=save_type, overwrite=overwrite, layers=layers, tilediff=tilediff, start=start)
+            for code in codes:
+                #generate qr code
+                data = code[0]
+                f_name = code[1]
+                qr = qrcode.QRCode(
+                    version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_L,
+                    box_size=10,
+                    border=4,
+                )
+                qr.add_data(data)
+                qr.make(fit=True)
+                img = qr.make_image(fill_color="black", back_color="white")
+                #make directory if it does not exist
+
+                os.makedirs(os.path.dirname(f_name), exist_ok=True)
+                img.save(f_name)
+                pass
+        else:
+            print(f"tag_id: {tag_id} not found in working.yaml")
+            #wait 10 seconds
             
- 
+            time.sleep(10)
 
 
-#take component positions from working_parts.csv and place them in working.kicad_pcb
-def place_parts(**kwargs):
-    board_file = "kicad/current_version/working/working.kicad_pcb"
-    parts_file = "working_parts.csv"
-    #load csv file
-    import csv
-    with open(parts_file, 'r') as f:
-        reader = csv.DictReader(f)
-        parts = [row for row in reader]
 
-
-    
-    oom_kicad.kicad_set_components(board_file=board_file, parts=parts, corel_pos=True, **kwargs)
-
+        #print(item)
+        #print(item["label"])
+        #print(item["template
 
 
 
 
 
 if __name__ == '__main__':
-    main()
+    #folder is the path it was launched from
+    
+    kwargs = {}
+    folder = os.path.dirname(__file__)
+    #folder = "C:/gh/oomlout_oomp_builder/parts"
+    #folder = "C:/gh/oomlout_oomp_part_generation_version_1/parts"
+    folder = "Z:\\oomlout_oomp_current_version_fast_test\\parts"
+    kwargs["folder"] = folder
+    main(**kwargs)
