6c16ade0c521fcb0c47906010bcedf2c01e6899d
Oom Lout <aaron@oomlout.com>
2024-11-11 13:03:41 +0000

    switched border size

--- working.py
+++ working.py
@@ -112,7 +112,7 @@ def generate_qr_code_generic(**kwargs):
                     version=1,
                     error_correction=qrcode.constants.ERROR_CORRECT_L,
                     box_size=10,
-                    border=4,
+                    border=0,
                 )
                 qr.add_data(data)
                 qr.make(fit=True)
